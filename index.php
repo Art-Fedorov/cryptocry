83fd51297071d4d8d0355992821f85a4fa84c3ac
Artem Fedorov <af@dsx.uk>
2017-12-24 00:23:16 +0300

    tz done without 5

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-header('Access-Control-Allow-Origin: *'); 
-$homepage = file_get_contents('https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-ETC');
+header('Access-Control-Allow-Origin: *');
+$url = null;
+if (isset($_POST['url'])){
+  $url = $_POST['url'];
+}
+if ($url != null)
+{
+  $content = file_get_contents($url);
+} else {
+  $content = file_get_contents('https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-ETC');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,18 +30,62 @@ $homepage = file_get_contents('https://bittrex.com/api/v1.1/public/getmarkethist
 <body>
   <div class="wrapper">
     <div class="container">
-      <div class="form-group row">
-        <label for="example-text-input" class="col-2 col-form-label">Text</label>
-        <div class="col-10">
-          <input class="form-control" type="text" id="url" value="" placeholder="Введите url">
+      <form class="form-group row col-xs-12 col-10" method="POST" action="index.php">
+        <div class="col-8">
+          <input class="form-control" type="text" name="url" id="url" value="<?php echo $url; ?>" placeholder="Введите url">
         </div>
+        <!-- <div class=""> -->
+          <button class="btn btn-primary col-4" type="submit">Go</button>
+        <!-- </div> -->
+      </form>
+      <div class="row form-group col-xs-12 col-10">
+        <label class="col-3 col-form-label">Фильтр по total</label>
+        <div class="col-6">
+          <input class="form-control" type="number" name="val" id="val" value="0" placeholder="Введите val">
+        </div>
+        <button class="btn btn-success col-3" id="filtering" type="button">Filter</button>
       </div>
 
       <div id="table"></div>
   <script>
     $(document).ready(function(){
-      var jsonArray = <?php echo $homepage; ?>;
+      var jsonArray = <?php echo $content; ?>;
       $.jsontotable(jsonArray.result, { id: '#table', className: 'table table-hover' });
+      $('#table tr').each(function(key,val){
+        var total = $(val).find('td:eq(4)');
+        var fillType = $(val).find('td:eq(5)');
+        fillType.swap(total);
+        
+        var orderType = $(val).find('td:eq(6)').text();
+        if (orderType.toLowerCase()=="buy"){
+          $(val).addClass('table__green');
+
+        } else if (orderType.toLowerCase()=="sell"){
+          $(val).addClass('table__red');
+
+        }
+      });
+      $('#filtering').click(function(){
+        $('#table tbody tr').each(function(key,val){
+          var total = $(val).find('td:eq(5)');
+          //var sel = $('#filterType option:selected').val();
+          var inputVal = $('#val').val();
+          //>=
+          //if (sel==0){
+            if (total.text()<inputVal) { $(val).hide(); }
+            else {
+              $(val).show();
+            }
+          //<=
+          // } else {
+          //     if (total.text()>inputVal) { $(val).hide(); }
+          //   else {
+          //     $(val).show();
+          //   }
+          // }
+        });
+      });
+      
     });
   </script>
     </div>
